610dc2ed4318107ec25ee76d063b83e73a3c2a9b
Dang Nguyen <dangnguyen@Dangs-MacBook-Pro.local>
2022-02-26 00:49:42 +0700

    first push code

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import Login from "./auth/Login.js";
+import React, { Component, Fragment } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Login />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/auth/Login.js
@@ -0,0 +1,47 @@
+import "./Login.scss";
+
+function Login() {
+  return (
+    <div className="login__background">
+      <div className="login">
+        <h1 className="login__title">Login</h1>
+        <button className="login__btnSocialLogin">
+          <i class="fa-brands fa-google login__socialIcon"></i>
+          <span className="login__socialText">Sign up with Google</span>
+        </button>
+        <div className="login__divider">
+          <span>Or</span>
+        </div>
+        <div className="login__form">
+          <input
+            className="login__form__input"
+            id="fullName"
+            type="text"
+            placeholder=" "
+          ></input>
+          <label className="login__form__label" for="fullName">
+            Full Name
+          </label>
+        </div>
+        <div className="login__form">
+          <input
+            className="login__form__input"
+            id="password"
+            type="password"
+            placeholder=" "
+          ></input>
+          <label className="login__form__label" for="password">
+            Password
+          </label>
+        </div>
+
+        <button className="login__submit">Login</button>
+        <p className="login__forgot">
+          <a href="#">Forgot your password ?</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
